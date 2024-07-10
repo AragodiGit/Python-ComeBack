a47cf52513d2780542ba8ff1962a6218d99ca92a
rakesh aragodi <rakesharagodi@gmail.com>
2024-07-10 19:42:45 +0530

    Adding Leetcode folder and adding python files solved on the leetcode

---
+++ Python VSCODE/Leetcode/Merge Strings Alternately.py
@@ -0,0 +1,36 @@
+"""You are given two strings word1 and word2. Merge the strings by adding letters in alternating order, starting with word1. If a string is longer than the other, append the additional letters onto the end of the merged string.
+Return the merged string
+
+Example 1:
+
+Input: word1 = "abc", word2 = "pqr"
+Output: "apbqcr"
+Explanation: The merged string will be merged as so:
+word1:  a   b   c
+word2:    p   q   r
+merged: a p b q c r
+
+Example 2:
+
+Input: word1 = "ab", word2 = "pqrs"
+Output: "apbqrs"
+Explanation: Notice that as word2 is longer, "rs" is appended to the end.
+word1:  a   b 
+word2:    p   q   r   s
+merged: a p b q   r   s"""
+
+class Solution:
+    def mergeAlternately(self, word1: str, word2: str) -> str:
+        len1 = len(word1)
+        len2 = len(word2)
+        long_len = [len1,len2]
+        long_len.sort()
+
+        output = ''
+
+        for i in range(long_len[-1]):
+            if i<len1:
+                output += word1[i]
+            if i<len2:
+                output += word2[i]
+        return output
